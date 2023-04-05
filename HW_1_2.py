7671d215e7fdcc97dd013ad8ef67c34975802afb
Nikos7702 <a7702b@gmail.com>
2023-04-05 13:18:42 +0200

    first commit

---
+++ HW_1_2.py
@@ -0,0 +1,10 @@
+x = float(input("enter a first integer: "))
+y = float(input("enter a second integer: "))
+
+print('addition: ', x + y)
+print('subtraction: ', x - y)
+print('multiplication: ', x * y)
+print('division: ', x / y)
+print('integer division: ', x // y)
+
+
