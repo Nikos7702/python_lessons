a94b3141c821b9e616e0284ff28cea0042a0bafb
Nikos7702 <a7702b@gmail.com>
2023-04-05 12:58:36 +0200

    first commit

--- HW_1_1.py
+++ HW_1_1.py
@@ -1,9 +1,9 @@
 # data input by user
-number1 = int(input("enter a first integer: "))
-number2 = int(input("enter a second integer: "))
-number3 = int(input("enter a third integer: "))
-number4 = int(input("enter a fourth integer: "))
-number5 = int(input("enter a fifth integer: "))
+number1 = int(input('enter a first integer: '))
+number2 = int(input('enter a second integer: '))
+number3 = int(input('enter a third integer: '))
+number4 = int(input('enter a fourth integer: '))
+number5 = int(input('enter a fifth integer: '))
 
 # find min, max, average
 minimum = min(number1, number2, number3, number4, number5)
@@ -11,6 +11,6 @@ maximum = max(number1, number2, number3, number4, number5)
 average = (number1 + number2 + number3 + number4 + number5) / 5
 
 # print results
-print(f"Min: {minimum}")
-print(f"Max: {maximum}")
-print(f"Average: {average}")
+print('Min: ', minimum)
+print('Max: ', maximum)
+print('Average: ', average)
