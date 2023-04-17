f1cefe590fca39206f6eda9d46d1b116e09e9ba6
Nikos7702 <a7702b@gmail.com>
2023-04-17 11:24:57 +0200

    first commit

---
+++ HW_4_2_10.py
@@ -0,0 +1,6 @@
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+total_sum = 0
+for row in matrix:
+    print(row)
+    total_sum += sum(row)
+print('Total sum of matrix elements:', total_sum)
