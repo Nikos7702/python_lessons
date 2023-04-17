1e35769d63440b01149114fc4dee41d80b7defe8
Nikos7702 <a7702b@gmail.com>
2023-04-17 11:22:52 +0200

    first commit

---
+++ HW_4_2_9.py
@@ -0,0 +1,6 @@
+salaries = [1500, 2000, 1800, 2500, 3000, 2200, 1900, 1700, 2300, 2100, 2400, 2800]
+total_salary = sum(salaries)
+average_salary = total_salary / len(salaries)
+print('Salaries:', salaries)
+print('Total salary:', total_salary)
+print('Average salary:', average_salary)
