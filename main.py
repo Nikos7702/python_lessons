af11726031908edd383a4ae4c52a178bd777907f
Nikos7702 <a7702b@gmail.com>
2023-05-09 20:10:13 +0200

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,71 @@
-from num2words import num2words
+class Product:
+    def __init__(self, title: str, price: float):
+        self.title = title
+        self.price = price
 
-num = 123.34
-num_str = num2words(num, lang='en')
-print(num_str)
+    def __str__(self):
+            return f'{self.product} {self.price}.'
+
+class Cart:
+
+    def __init__(self):
+        self.products = []
+        def __str__(self):
+            return f'\n'.join(map(str, self.products))
+
+    def add_student(self, product: Product):
+        self.products.append(product)
+product_1 = Product('Tomato', 13.6)
+product_2 = Product('Cucumber', 9)
+product_3 = Product('Milk', 16)
+
+
+
+cart = Cart()
+print(cart)
+
+
+
+
+
+class Student:
+
+    def __init__(self, name: str, surname: str, date_of_birth: str):
+        self.name = name
+        self.surname = surname
+        self.date_of_birth = date_of_birth
+
+    def __str__(self):
+        return f'{self.surname} {self.name[0]}.'
+
+
+class Group:
+
+    def __init__(self, title: str):
+        self.title = title
+        self.students = []
+
+    def add_student(self, student: Student):
+        self.students.append(student)
+
+    def __len__(self):
+        return len(self.students)
+
+    def __str__(self):
+        return f'{self.title}\n' + '\n'.join(map(str, self.students))
+
+
+group = Group('Python Pro')
+student_1 = Student('Ivan1', 'Ivanov1', '12.03.2001')
+student_2 = Student('Ivan2', 'Ivanov2', '12.03.2002')
+student_3 = Student('Ivan3', 'Ivanov3', '12.03.2003')
+student_4 = Student('Ivan4', 'Ivanov4', '12.03.2004')
+student_5 = Student('Ivan5', 'Ivanov5', '12.03.2005')
+
+group.add_student(student_1)
+group.add_student(student_2)
+group.add_student(student_3)
+group.add_student(student_4)
+group.add_student(student_5)
+
+print(group)
---
+++ python_pro/HW_1_1.py
@@ -0,0 +1,56 @@
+"""
+Створіть клас Product для опису товару. У якості атрибутів товару можна використовувати назву, цінц та опис товару.
+Створіть декілька інстансів класу Product.
+Створіть клас Cart, який буде виступати у якості кошика з товарами певного покупця.
+Кошик може містити декілька товарів певної кількості. Реалізуйте метод обчислення вартості кошика.
+В усіх класах має бути визначений метод str.
+"""
+class Product:
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
+
+    def __str__(self):
+        return f"Назва: {self.name}\n Ціна: {self.price} грн\n Опис: {self.description}"
+
+
+class Cart:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product, quantity=1):
+        self.products.append((product, quantity))
+
+    def calculate_total(self):
+        total = 0
+        for item in self.products:
+            product, quantity = item
+            total += product.price * quantity
+        return total
+
+    def __str__(self):
+        cart_info = "Кошик:\n"
+        for item in self.products:
+            product, quantity = item
+            cart_info += f"Назва: {product.name}, Кількість: {quantity}, Ціна: {product.price} грн\n"
+        cart_info += f"Загальна вартість: {self.calculate_total()} грн"
+        return cart_info
+
+
+# Створення екземплярів класу "Product"
+product1 = Product("Ноутбук", 20000, "Потужний ноутбук для роботи")
+product2 = Product("Смартфон", 10000, "Сучасний смартфон з високоякісною камерою")
+
+# Виведення інформації про товари
+print(product1)
+print(product2)
+
+
+cart = Cart()
+cart.add_product(product1, 2)
+cart.add_product(product2, 1)
+
+# Виведення інформації про кошик та його загальної вартості
+print(cart)
+
