2ed03e70af6ce4dea116ffa0d70f682c57eaef37
Nikos7702 <a7702b@gmail.com>
2023-06-20 15:22:21 +0200

    first commit

--- python_pro/HW_6_3_2.py
+++ python_pro/HW_6_3_2.py
@@ -1,3 +1,18 @@
 """
 Реалізуйте свій аналог генераторної функції range().
 """
+def my_range(start, stop=None, step=1):
+    if stop is None:
+        stop = start
+        start = 0
+
+    current = start
+    while current < stop:
+        yield current
+        current += step
+
+
+for item in my_range(10):
+    print(item)
+
+
