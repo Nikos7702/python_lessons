ed3fd9be71433c121802a9431691c1b5b50216bb
Nikos7702 <a7702b@gmail.com>
2023-06-30 14:17:34 +0200

    first commit

---
+++ python_pro/HW_8_5.py
@@ -0,0 +1,21 @@
+"""
+Напишіть декоратор, який логує аргументи та результати функції.
+"""
+def log_arguments_results(func):
+    def inner(*args, **kwargs):
+        print(func.__name__)
+        print("ARGS:", args)
+        print("Кwargs:", kwargs)
+
+        result = func(*args, **kwargs)
+
+        print("Result:", result)
+
+
+    return inner
+
+@log_arguments_results
+def add_numbers(a, b):
+    return a + b
+
+add_numbers(3, 4)
